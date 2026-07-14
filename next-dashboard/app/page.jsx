22851abf14a037bf1b8dcb418863a641ec156031
Suchit <suchitprabuvenkatesh@gmail.com>
2026-07-14 20:03:50 +0530

    feat: improved dashboard

--- next-dashboard/app/page.jsx
+++ next-dashboard/app/page.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:602862d83d8019642e85a7105ec9cd13d2c2bf41c57ea5a930d521b4c7ced571
-size 21103
+oid sha256:5fbe38baaae0613db31a5d3e601ee8f647cd54dca66235255c97ae1b80e4e6ec
+size 21093
