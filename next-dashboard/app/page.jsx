4a363bfda96e3f5ea21b2e8d0d03e8a237eb250c
Suchit <suchitprabuvenkatesh@gmail.com>
2026-07-17 23:31:00 +0530

    Dashboard UI enhancements: dynamic active violation top alerts and enlarged video feed

--- next-dashboard/app/page.jsx
+++ next-dashboard/app/page.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e1a0dda9e61810bd576e4cd2da30d28c105519573bbabf44f48bb70c5c764a3c
-size 21157
+oid sha256:ea3210bf531dffbc1599b1947155731d9f1d9a002f9fca1e840db940964b497e
+size 20614
